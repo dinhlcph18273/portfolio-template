fa6165e337ce4bea440f574914d6323e86c57a7f
Lê Công Định <hobao3210@gmail.com>
2022-05-11 08:49:32 +0700

    commit

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api"
+    baseURL: "https://node-portpolio.herokuapp.com/api"
 }
 )
 export default instance

4f3b3d72e20b4d38e872df217e7548302d1acd24
Lê Công Định <hobao3210@gmail.com>
2022-06-12 00:14:52 +0700

    commit img

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
             <div style={{ padding: 16 }}>
                 <div className='header-top'>
                     <h1><Link to="" style={{ color: 'white', fontWeight: 800 }}>Le Cong Dinh</Link></h1>
-                    <img src="https://scontent.fhan3-5.fna.fbcdn.net/v/t1.15752-9/278979470_716705392853562_1086265818398991727_n.png?_nc_cat=110&ccb=1-5&_nc_sid=ae9488&_nc_ohc=zNIBLmbQYZsAX8yR5Xu&tn=ZtNM5bzHUEq2g-oy&_nc_ht=scontent.fhan3-5.fna&oh=03_AVK3eZM7tvQfZm34SHNk4e-dToX-k-RXknhns7zLdlR56Q&oe=62989372" width={200} height={200} alt="image"></img>
+                    <img src="https://res.cloudinary.com/dinhlcph18273/image/upload/v1654967495/153620650_1259363304459853_3632080232479997762_n_odxwfa.png" width={200} height={200} alt="image"></img>
                     <p style={{ color: 'white', padding: 10 }}>Hi, my name is Dinh and I'm a front end developer. Welcome to my personal website!</p>
                     <ul className='menu'>
                         <li><a target={`_blank`} href="https://www.instagram.com/polyintel/"><Button icon={<InstagramFilled />} ></Button> </a></li>
